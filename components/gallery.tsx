7189872e48c73117315546305a578beab741090d
padash00 <76392786+padash00@users.noreply.github.com>
2025-02-17 05:23:08 +0500

    Update gallery.tsx

--- components/gallery.tsx
+++ components/gallery.tsx
@@ -26,11 +26,6 @@ const images = [
     category: "zones",
   },
   {
-    src: "https://i.postimg.cc/4N5CYn9m/DSCF2919.png",
-    alt: "Киберспортивная арена",
-    category: "tournaments",
-  },
-  {
     src: "https://images.unsplash.com/photo-1598550476439-6847785fcea6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
     alt: "Геймеры в наушниках",
     category: "players",
