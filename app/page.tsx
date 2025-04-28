1fad9d5dde36c934a1c3500e431aaaee887e1e97
padash00 <76392786+padash00@users.noreply.github.com>
2025-04-28 23:01:49 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1074,7 +1074,25 @@
            </div>
          </div>
        </section>
- 
+      <>
+  {/* Yandex.Metrika counter */}
+  <script type="text/javascript" dangerouslySetInnerHTML={{
+    __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();
+    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+    ym(101463564, "init", {
+      clickmap:true,
+      trackLinks:true,
+      accurateTrackBounce:true,
+      webvisor:true,
+      ecommerce:"dataLayer"
+    });`
+  }} />
+  <noscript><div><img src="https://mc.yandex.ru/watch/101463564" style={{position:"absolute", left:"-9999px"}} alt="" /></div></noscript>
+  {/* /Yandex.Metrika counter */}
+</>
+
        {/* Подвал */}
        <footer className="py-10 border-t border-border-color">
          <div className="container mx-auto px-4">
